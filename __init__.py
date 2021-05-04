d7fb4fd929ad12cc4bb2068a7d3bc1c5f4e907f0
Xunmo Yang <xunmo@google.com>
2021-05-04 15:52:17 -0700

    Add Google Analytics tracking for Meterstick import.
    PiperOrigin-RevId: 372012131

--- __init__.py
+++ __init__.py
@@ -27,3 +27,25 @@ except (ImportError, ModuleNotFoundError) as e:
       'WARNING: metersick.model is not imported because: "%s". '
       "It's OK if you don't fit model in Meterstick."
       % repr(e))
+try:
+  # Send a request to Google Analytics to track usage.
+  import requests  # pylint: disable=g-import-not-at-top
+  import uuid  # pylint: disable=g-import-not-at-top
+
+  data = {
+      'v': '1',  # API Version.
+      'tid': 'UA-189724257-3',  # Tracking ID / Property ID.
+      # Anonymous Client Identifier. Ideally, this should be a UUID that
+      # is associated with particular user, device, or browser instance.
+      'cid': str(uuid.uuid4()),
+      't': 'event',  # Event hit type.
+      'ec': 'import',  # Event category.
+      'ea': 'import',  # Event action.
+      'el': 'external import',  # Event label.
+      'ev': 1,  # Event value, must be an integer
+      'ua': '',  # User agent
+  }
+  response = requests.post(
+      'https://www.google-analytics.com/collect', data=data)
+except:  # pylint: disable=bare-except
+  pass
